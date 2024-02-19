c415e95f54f58518caa43dd939a7052445289a28
davidAllenStephan <david.marino03@hotmail.com>
2024-02-19 15:32:11 -0600

    added prototypes

---
+++ src/edu/mu/VehicleManager.java
@@ -0,0 +1,95 @@
+package edu.mu;
+
+public class VehicleManager {
+	
+	//Ash
+	public void VehicleManager(String fileName) {
+		
+	}
+	
+	//Syd
+	public boolean readFromFile(String fileName) {
+		return true;
+	}
+	
+	//Syd
+	public void displayAllCarInformation() {
+		
+	}
+	
+	//Syd
+	public void displayAllTruckInformation() {
+		
+	}
+	
+	//Syd
+	public void displayAllSUVInformation() {
+		
+	}
+	
+	//Syd
+	public void displayAllMotorBikeInformation() {
+		
+	}
+	
+	//Syd
+	public void displayVehicleInformation(Vehicle v) {
+		
+	}
+	
+	//Syd
+	public void displayAllVehicleInformation() {
+		
+	}
+	
+	//Ash
+	public boolean removeVehicle(Vehicle vehicle) {
+		return true;
+	}
+	
+	//Ash
+	public boolean addVehicle(Vehicle vehicle) {
+		return true;
+	}
+	
+	//Ash
+	public boolean saveVehicleList() {
+		return true;
+	}
+	
+	//Isaiah
+	private boolean isVehicleType(Vehicle v, Class class) {
+		return true;
+	}
+	
+	//Isaiah
+	public int getNumberOfVehichlesByType(Class class) {
+		
+	}
+	
+	//David
+	public Vehicle getVehicleWithHighestMaintenanceCost(double distance) {
+		
+	}
+	
+	//David
+	public Vehicle getVehicleWithLowestMaintenanceCost(double distance) {
+		
+	}
+	
+	//David
+	public ArrayList<Vehicle> getVehicleWithHighestFuelEfficiency(double distance, double fuelPrice) {
+		
+	}
+	
+	//David
+	public ArrayList<Vehicle> getVehicleWithLowestFuelEfficiency(double distance, double fuelPrice) {
+		
+	}
+	
+	//David
+	public double getAverageFuelEfficiencyOfSUVs(double distance, double fuelPrice) {
+		
+	}
+	
+}
