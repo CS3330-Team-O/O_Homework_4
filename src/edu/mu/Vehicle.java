2a0554b14de16ed26bd113f0618b6064c867db39
Ashley OGorman <ashley87105@gmail.com>
2024-02-20 19:32:12 -0600

    add readFromFile() and conversion to enum methods

--- src/edu/mu/Vehicle.java
+++ src/edu/mu/Vehicle.java
@@ -1,5 +1,47 @@
 package edu.mu;
 
 public class Vehicle {
-
+	
+	public static FuelType convertStringToFuelType(String fuelType) {
+		switch (fuelType) {
+			case "GASOLINE":
+				return FuelType.GASOLINE;
+			case "DIESEL":
+				return FuelType.DIESEL;
+			case "ELECTRIC":
+				return FuelType.ELECTRIC;
+			default:
+				return FuelType.HYBRID;
+		}
+	}
+	
+	public static StartMechanism convertStringToStartMechanism(String startType) {
+		switch (startType) {
+			case "KEYSTART":
+				return StartMechanism.KEYSTART;
+			case "KICKSTART":
+				return StartMechanism.KICKSTART;
+			default:
+				return StartMechanism.PUSHSTART;
+		}
+	}
+	
+	public static VehicleColor convertStringToVehicleColor(String color) {
+		switch (color) {
+			case "WHITE":
+				return VehicleColor.WHITE;
+			case "BLACK":
+				return VehicleColor.BLACK;
+			case "RED":
+				return VehicleColor.RED;
+			case "YELLOW":
+				return VehicleColor.YELLOW;
+			case "GRAY":
+				return VehicleColor.GRAY;
+			case "BLUE":
+				return VehicleColor.BLUE;
+			default:
+				return VehicleColor.BROWN;
+		}
+	}
 }
