685c5d8eafc88ae21bf4d25fa7d729b7826ebd87
Ashley OGorman <ashley87105@gmail.com>
2024-02-21 18:32:16 -0600

    update Main

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -6,8 +6,9 @@ public class Main {
 		VehicleManager vehicleManager = new VehicleManager();
 		boolean readFile = vehicleManager.readFromFile("vehicleList.csv");
 		vehicleManager.VehicleManager("vehicleList.csv");
-		boolean remove = vehicleManager.removeVehicle(null);
-		boolean add = vehicleManager.addVehicle(null);
+		Car carTest = new Car("Test", "Test", 2024, 100000, VehicleColor.BLUE, FuelType.DIESEL, 50000, 3.0, 4, 20, StartMechanism.KEYSTART);
+		boolean add = vehicleManager.addVehicle(carTest);
+		boolean remove = vehicleManager.removeVehicle(carTest);
 		boolean save = vehicleManager.saveVehicleList();
 	}
 
