42b2a6259da4016c65923c8ea37841d009174682
davidAllenStephan <david.marino03@hotmail.com>
2024-02-23 14:50:39 -0600

    fixed problems with querying for fuel economy and maintence

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -1,22 +1,27 @@
 package edu.mu;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		VehicleManager vehicleManager = new VehicleManager();
-		boolean readFile = vehicleManager.readFromFile("vehicleList.csv");
-		vehicleManager.VehicleManager("vehicleList.csv");
+		VehicleManager vehicleManager = new VehicleManager("vehicleList.csv");
 		Car carTest = new Car("Test", "Test", 2024, 100000, VehicleColor.BLUE, FuelType.DIESEL, 50000, 3.0, 4, 20, StartMechanism.KEYSTART);
 		boolean add = vehicleManager.addVehicle(carTest);
 		boolean remove = vehicleManager.removeVehicle(carTest);
-		boolean save = vehicleManager.saveVehicleList();
-		
 		vehicleManager.displayAllCarInformation();
-    vehicleManager.getVehicleWithHighestMaintenanceCost(0);
-		vehicleManager.getVehicleWithLowestMaintenanceCost(0);
-		vehicleManager.getVehicleWithHighestFuelEfficiency(0, 0);
-		vehicleManager.getVehicleWithLowestFuelEfficiency(0, 0);
-		vehicleManager.getAverageFuelEfficiencyOfSUVs(0, 0);
+		Vehicle highestMaintenanceCostVehicle = vehicleManager.getVehicleWithHighestMaintenanceCost(100);
+		System.out.println("highestMaintenanceCostVehicle: " + highestMaintenanceCostVehicle);
+		Vehicle lowestMaintenanceCostVehicle =vehicleManager.getVehicleWithLowestMaintenanceCost(100);
+		System.out.println("lowestMaintenanceCostVehicle: " + lowestMaintenanceCostVehicle);
+		ArrayList<Vehicle> highestFuelEfficiencyVehicles = vehicleManager.getVehicleWithHighestFuelEfficiency(100, 100);
+		System.out.println("highestFuelEfficiencyVehicle: " + highestFuelEfficiencyVehicles);
+		ArrayList<Vehicle> lowestFuelEfficiencyVehicles = vehicleManager.getVehicleWithLowestFuelEfficiency(100, 100);
+		System.out.println("lowestFuelEfficiencyVehicle: " + lowestFuelEfficiencyVehicles);
+		double averageFuelEfficiencyOfSUVs = vehicleManager.getAverageFuelEfficiencyOfSUVs(100, 100);
+		System.out.println("averageFuelEfficiencyOfSUVs: " + averageFuelEfficiencyOfSUVs);
+		
+		boolean save = vehicleManager.saveVehicleList();
 	}
 
 }
--- src/edu/mu/SUV.java
+++ src/edu/mu/SUV.java
@@ -6,6 +6,7 @@ public class SUV extends Vehicle{
 	}
 	
 	public SUV(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType, double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
+		super.type = "SUV";
 		super.brand = brand;
 		super.make = make;
 		super.color = color;
