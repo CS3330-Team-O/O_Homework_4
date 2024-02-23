98804d6a7796ac80cbeb07b7cab966d25c19bc52
Sydney Poulsen <sydpoulsen@yahoo.com>
2024-02-23 14:21:40 -0600

    edited order of main

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -8,15 +8,17 @@ public class Main {
 		vehicleManager.VehicleManager("vehicleList.csv");
 		Car carTest = new Car("Test", "Test", 2024, 100000, VehicleColor.BLUE, FuelType.DIESEL, 50000, 3.0, 4, 20, StartMechanism.KEYSTART);
 		boolean add = vehicleManager.addVehicle(carTest);
+		vehicleManager.displayVehicleInformation(carTest);
 		boolean remove = vehicleManager.removeVehicle(carTest);
 		boolean save = vehicleManager.saveVehicleList();
+		
+		
 		
 		vehicleManager.displayAllCarInformation();
 		vehicleManager.displayAllTruckInformation();
 		vehicleManager.displayAllSUVInformation();
 		vehicleManager.displayAllMotorBikeInformation();
 		vehicleManager.displayAllVehicleInformation();
-		vehicleManager.displayVehicleInformation(carTest);
 		
 		vehicleManager.getVehicleWithHighestMaintenanceCost(0);
 		vehicleManager.getVehicleWithLowestMaintenanceCost(0);
