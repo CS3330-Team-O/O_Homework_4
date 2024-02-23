a66e127be6b9786b6a96d0d8bff35561c0d73dca
Sydney Poulsen <sydpoulsen@yahoo.com>
2024-02-23 14:41:09 -0600

    edited main

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -5,16 +5,32 @@ public class Main {
 	public static void main(String[] args) {
 		VehicleManager vehicleManager = new VehicleManager();
 		boolean readFile = vehicleManager.readFromFile("vehicleList.csv");
-		vehicleManager.VehicleManager("vehicleList.csv");
+		System.out.println("-------------Original Car Info-------------\n");
+		vehicleManager.displayAllCarInformation();
+		
 		Car carTest = new Car("Test", "Test", 2024, 100000, VehicleColor.BLUE, FuelType.DIESEL, 50000, 3.0, 4, 20, StartMechanism.KEYSTART);
 		boolean add = vehicleManager.addVehicle(carTest);
+<<<<<<< HEAD
 		vehicleManager.displayVehicleInformation(carTest);
 		boolean remove = vehicleManager.removeVehicle(carTest);
 		boolean save = vehicleManager.saveVehicleList();
 		
 		
 		
+=======
+		System.out.println("-------------Car Info with Test-------------\n");
+>>>>>>> 6244d68b804414a9e42b94eb6084cd98e8c2cbb2
 		vehicleManager.displayAllCarInformation();
+		
+		boolean remove = vehicleManager.removeVehicle(carTest);
+		System.out.println("-------------Car Info without Test-------------\n");
+		vehicleManager.displayAllCarInformation();
+		
+		boolean save = vehicleManager.saveVehicleList();
+		System.out.println("-------------Reading info in after save to CSV-------------\n");
+		vehicleManager.VehicleManager("vehicleList.csv");
+		vehicleManager.displayAllCarInformation();
+
 		vehicleManager.displayAllTruckInformation();
 		vehicleManager.displayAllSUVInformation();
 		vehicleManager.displayAllMotorBikeInformation();
